03f30ab6e84b35918684c3f40c3613ab04236038
jgabriel1 <josegabriel@det.ufc.br>
2020-09-01 23:25:25 -0300

    Able to delete transactions.

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -36,10 +36,13 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // const { id: transactionID } = request.params;
-  // const deleteTransaction = new DeleteTransactionService();
-  // await deleteTransaction.execute(transactionID);
-  // return response.status(204).send();
+  const { id: transactionId } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute({ transactionId });
+
+  return response.status(204).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,37 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  transactionId: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  private transactionsRepository: TransactionsRepository;
+
+  constructor() {
+    this.transactionsRepository = getCustomRepository(TransactionsRepository);
+  }
+
+  private async getTransactionById(
+    transactionId: string,
+  ): Promise<Transaction | null> {
+    const transaction = await this.transactionsRepository.findOne({
+      where: { id: transactionId },
+    });
+
+    return transaction || null;
+  }
+
+  public async execute({ transactionId }: Request): Promise<void> {
+    const transactionToBeDeleted = await this.getTransactionById(transactionId);
+
+    if (!transactionToBeDeleted) {
+      throw new AppError("Transaction doesn't exist.", 404);
+    }
+
+    await this.transactionsRepository.remove(transactionToBeDeleted);
   }
 }
 
